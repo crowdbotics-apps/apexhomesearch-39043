2e85a8a1762bd5c905a653dea29088c662234be9
Justin Heminger <justin@apexrealtygroup.info>
2023-02-20 14:07:10 -0900

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "apexhomesearch-39043",
   url: "https://apexhomesearch-39043.botics.co",
   api: "https://apexhomesearch-39043.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
